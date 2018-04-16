5a3f10fdf4ee1869670f3a90687eef4705294b4f
Jim <reidj32@gmail.com>
2018-04-16 09:03:59 -0500

    adding the angular project

---
+++ src/angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jpr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'jpr';
+}
