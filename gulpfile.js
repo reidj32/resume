b002efd0313d0e4373ea52bd5961d1be7c879900
Jim Reid <reidj32@gmail.com>
2018-05-02 13:12:57 -0500

    setup gulp for different environments

--- gulpfile.js
+++ gulpfile.js
@@ -7,118 +7,139 @@ var htmlmin = require('gulp-htmlmin');
 var sequence = require('gulp-sequence');
 var zip = require('gulp-zip');
 var webserver = require('gulp-webserver');
-var exec = require('child_process').exec;
+var gulpif = require('gulp-if');
+var child_process = require('child_process');
+var yargs = require('yargs');
+
+var config = {
+  environment: yargs.argv.env || 'development',
+  development() {
+    return this.environment === 'development' || this.environment === 'dev';
+  },
+  production() {
+    return this.environment === 'production' || this.environment === 'prod';
+  }
+};
+
+var webserverOpts = {
+  open: true,
+  livereload: true,
+  fallback: 'index.html'
+};
+
+var htmlminOpts = {
+  collapseWhitespace: true,
+  removeComments: true
+};
+
+var watchOpts = {
+  delay: 100
+};
 
 /**
  * Cleans and builds the entire project
  */
-gulp.task('default', sequence('clean', 'build'));
+gulp.task('default', ['build']);
 
 /**
  * After building all projects, zips up the dist/ folder for deployment
  */
-gulp.task('bundle', ['build'], function() {
-  gulp.src('./dist/**/*')
-      .pipe(zip('bundle.zip'))
-      .pipe(gulp.dest('./dist/'));
+gulp.task('bundle', ['package'], function() {
+  return gulp.src('./dist/**/*')
+    .pipe(zip('bundle.zip'))
+    .pipe(gulp.dest('./dist/'));
 });
 
 /**
  * Builds all projects
  */
-gulp.task('build', ['build-welcome', 'build-html5', 'build-angular']);
+gulp.task('build', sequence('clean', ['build:welcome', 'build:html5', 'build:angular']));
 
 /**
  * Packages all files and place them in the ./dist/ directory
  */
-gulp.task('package', ['package-welcome', 'package-html5', 'package-angular']);
+gulp.task('package', sequence('clean', ['package:welcome', 'package:html5', 'package:angular']));
 
 /**
 * Cleans the project output files
 */
-gulp.task('clean', ['clean-welcome', 'clean-html5', 'clean-angular'], function() {
+gulp.task('clean', ['clean:welcome', 'clean:html5', 'clean:angular'], function() {
    del.sync(['./dist']);
 });
 
 /**
- * Builds the welcome project
+ * Builds the Welcome project
  */
-gulp.task('build-welcome', ['build-welcome-html', 'build-welcome-styles', 'build-welcome-images']);
+gulp.task('build:welcome', ['build:welcome:html', 'build:welcome:styles', 'build:welcome:assets']);
 
 /**
- * Minify the welcome html files.
+ * Minify the Welcome html files.
  */
-gulp.task('build-welcome-html', function() {
+gulp.task('build:welcome:html', function() {
   return gulp.src('./src/welcome/src/**/*.html')
-    .pipe(htmlmin({
-        collapseWhitespace: true,
-        removeComments: true
-    }))
+    .pipe(gulpif(config.production(), htmlmin(htmlminOpts)))
     .pipe(gulp.dest('./src/welcome/dist/'));
 });
 
 /**
- * Minify the welcome css files.
+ * Minify the Welcome css files.
  */
-gulp.task('build-welcome-styles', function() {
+gulp.task('build:welcome:styles', function() {
   return gulp.src('./src/welcome/src/**/*.css')
-    .pipe(uglifycss())
+    .pipe(gulpif(config.production(), uglifycss()))
     .pipe(gulp.dest('./src/welcome/dist/'));
 });
 
 /**
- * Copy the welcome img files.
+ * Copy the Welcome asset files.
  */
-gulp.task('build-welcome-images', function() {
+gulp.task('build:welcome:assets', function() {
   return gulp.src(['./src/welcome/src/**/*.?(png|jpg|ico)'])
     .pipe(gulp.dest('./src/welcome/dist/'));
 });
 
 /**
- * Packages the welcome build
+ * Packages the Welcome build
  */
-gulp.task('package-welcome', ['build-welcome'], function() {
+gulp.task('package:welcome', ['build:welcome'], function() {
   return gulp.src('./src/welcome/dist/**/*')
     .pipe(gulp.dest('./dist/'));
 });
 
 /**
- * Cleans the welcome project
+ * Cleans the Welcome project
  */
-gulp.task('clean-welcome', function() {
+gulp.task('clean:welcome', function() {
   del.sync(['./src/welcome/dist']);
 });
 
 /**
  * Builds the HTML5 project
  */
-gulp.task('build-html5', ['build-html5-html', 'build-html5-styles']);
+gulp.task('build:html5', ['build:html5:html', 'build:html5:styles']);
 
 /**
  * Minify the HTML5 project html files
  */
-gulp.task('build-html5-html', function() {
+gulp.task('build:html5:html', function() {
   return gulp.src('./src/html5/src/**/*.html')
-    .pipe(htmlmin({
-        collapseWhitespace: true,
-        removeComments: true
-    }))
+    .pipe(gulpif(config.production(), htmlmin(htmlminOpts)))
     .pipe(gulp.dest('./src/html5/dist/'));
 });
 
 /**
  * Minify the HTML5 project css files
  */
-gulp.task('build-html5-styles', function() {
+gulp.task('build:html5:styles', function() {
   return gulp.src('./src/html5/src/**/*.css')
-    .pipe(uglifycss())
+    .pipe(gulpif(config.production(), uglifycss()))
     .pipe(gulp.dest('./src/html5/dist/'));
 });
 
 /**
  * Packages the HTML5 build
  */
-gulp.task('package-html5', ['build-html5'], function() {
+gulp.task('package:html5', ['build:html5'], function() {
   return gulp.src('./src/html5/dist/**/*')
     .pipe(gulp.dest('./dist/resumes/html5/'));
 });
@@ -126,25 +147,33 @@ gulp.task('package-html5', ['build-html5'], function() {
 /**
  * Cleans the HTML5 project
  */
-gulp.task('clean-html5', function() {
+gulp.task('clean:html5', function() {
   del.sync(['./src/html5/dist']);
 });
 
 /**
  * Builds the Angular project
  */
-gulp.task('build-angular', function(cb) {
-  exec('ng build --prod --base-href /resumes/angular/ --deploy-url /resumes/angular/', function(err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('build:angular', function(cb) {
+  if (config.development()) {
+    child_process.exec('ng build --base-href /resumes/angular/ --deploy-url /resumes/angular/', function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  } else if (config.production()) {
+    child_process.exec('ng build --prod --base-href /resumes/angular/ --deploy-url /resumes/angular/', function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  }
 });
 
 /**
  * Packages the Angular build
  */
-gulp.task('package-angular', ['build-angular'], function() {
+gulp.task('package:angular', ['build:angular'], function() {
   return gulp.src('./src/angular/dist/**/*')
     .pipe(gulp.dest('./dist/resumes/angular/'));
 });
@@ -152,7 +181,7 @@ gulp.task('package-angular', ['build-angular'], function() {
 /**
  * Cleans the Angular project
  */
-gulp.task('clean-angular', function() {
+gulp.task('clean:angular', function() {
   del.sync(['./src/angular/dist']);
 });
 
@@ -161,39 +190,33 @@ gulp.task('clean-angular', function() {
  */
 gulp.task('run', ['build'], function() {
   return gulp.src('./dist')
-    .pipe(webserver({
-      open: true,
-      fallback: 'index.html'
-    }));
+    .pipe(webserver(webserverOpts))
+    .pipe(gulp.watch('./src/?(welcome|html5|angular)/src/**/*', watchOpts, ['build']));
 });
 
 /**
  * Runs the welcome project in a local webserver
  */
-gulp.task('run-welcome', ['build-welcome'], function() {
+gulp.task('run:welcome', ['build:welcome'], function() {
   return gulp.src('./src/welcome/dist')
-    .pipe(webserver({
-      open: true,
-      fallback: 'index.html'
-    }));
+    .pipe(webserver(webserverOpts))
+    .pipe(gulp.watch('./src/welcome/src/**/*', watchOpts, ['build:welcome']));
 });
 
 /**
  * Runs the HTML5 project in a local webserver
  */
-gulp.task('run-html5', ['build-html5'], function() {
+gulp.task('run:html5', ['build:html5'], function() {
   return gulp.src('./src/html5/dist')
-    .pipe(webserver({
-      open: true,
-      fallback: 'index.html'
-    }));
+    .pipe(webserver(webserverOpts))
+    .pipe(gulp.watch('./src/html5/src/**/*', watchOpts, ['build:html5']));
 });
 
 /**
  * Runs the Angular project in a local webserver
  */
-gulp.task('run-angular', function() {
-  exec('ng serve --open', function(err, stdout, stderr) {
+gulp.task('run:angular', function() {
+  child_process.execSync('ng serve --open', function(err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
   });
