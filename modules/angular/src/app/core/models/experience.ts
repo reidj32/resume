0f56fc5acb293c0ee2aea6c158eb1790f97d7dc1
Jim Reid <reidj32@gmail.com>
2018-05-29 14:52:45 -0500

    (fix): correcting model bug for experience history

--- modules/angular/src/app/core/models/experience.ts
+++ modules/angular/src/app/core/models/experience.ts
@@ -2,5 +2,5 @@ import { History } from './history';
 
 export class Experience {
   title: string;
-  history: History;
+  history: History[] = [];
 }
--- modules/angular/src/app/core/models/history.ts
+++ modules/angular/src/app/core/models/history.ts
@@ -3,6 +3,7 @@ import { Position } from './position';
 
 export class History {
   company: string;
+  description: string;
   contact: Contact;
   positions: Position[] = [];
 }
