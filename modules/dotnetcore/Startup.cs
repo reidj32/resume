89855130a912665f505739e7b2cbb18de23cb8af
Jim <reidj32@gmail.com>
2018-05-29 22:38:51 -0500

    enable forwarded headers

--- modules/dotnetcore/Startup.cs
+++ modules/dotnetcore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Resume.Services;
@@ -39,6 +40,11 @@ namespace Resume
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseStaticFiles();
 
